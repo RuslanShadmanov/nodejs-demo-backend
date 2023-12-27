8947a186be49fec54afcfe888b1a9abd5725f1a9
Ruslan <shadmanov.ruslan@gmail.com>
2023-12-27 11:49:57 -0800

    add get by id

--- server.js
+++ server.js
@@ -1,30 +1,52 @@
-import  express  from "express";
+import  express, { json }  from "express";
 import {v4 as uuid} from "uuid";
+import {readFile,writeFile} from "node:fs/promises";
 
 const server = express()
 
 const port = process.env.PORT || 8000;
 
-console.log(process.env);
+
 
 server.use (express.json())
 
 server.get('/', async(req,res)=>{
     res.json({message:"Yes home is working"})
 })
-server.get('/users',async (req,res)=>{
+server.get("/users",async (req,res)=>{
     try{
-    const response = await fetch ("https://jsonplaceholder.typicode.com/users")
-    const data = await response.json()
+  const data = await readFile("./data/users.json", { encoding: 'utf8' })
    
-    res.status(200).end(JSON.stringify(data));
+    const users =JSON.parse(data);
+    res.json(users);
     } catch (error){
-        throw Error ("Something wrong",error)
+        res.json({error:"Something went wrong"});
+         
     }
 
-})
+});
 
-server.get('/users/:id',()=>{})
+server.get('/users/:id',async(req,res)=>{
+const id = req.params.id;
+try{
+    const data = await readFile("./data/users.json", { encoding: 'utf8' })
+    const users = JSON.parse(data);
+     
+      const singleUser=users.find((user)=>user.id===id)
+      if (singleUser){
+          res.json(singleUser)
+        }else{
+        res.json({error:"User not found"})
+
+      }
+      
+      } catch (error){
+          res.json({error:"Something went wrong"});
+           
+      }
+  
+    });
+
 server.delete('/users/:id',()=>{})
 server.post('/',()=>{})
 server.patch('/',()=>{})
